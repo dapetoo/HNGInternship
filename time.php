eb1e5171429ef5d358eb27a11cc9db974a4c5691
Peter <dapetoo@gmail.com>
2018-04-18 03:36:47 +0100

    This is a time function in PHP

---
+++ time.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Figma Design</title>
+	<style type="text/css">
+		#body{
+			background: red;
+		}
+	</style>
+</head>
+<body id="body">
+	<h1>HNG Internship 4.0</h2>
+<?php
+echo "The time is " . date("h:i:sa");
+echo "<br/>";
+?>
+<a href="twitter.com/hnginternship"> Follow HNG on Twitter</a>
+</body>
+</html>
